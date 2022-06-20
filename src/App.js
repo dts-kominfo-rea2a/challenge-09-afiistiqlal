8e00a17e0a3773106195d0d653f6da5a1cc65a7b
afiistiqlal <afiistiqlal@gmail.com>
2022-06-21 03:55:44 +0700

    [Update] error keys

--- src/App.js
+++ src/App.js
@@ -7,12 +7,13 @@ import Header from './components/Header';
 
 const App = () => {
   // Masukkan Header dan Contact ke dalam div App
+  const contactList = contacts.map(contact => 
+    <Contact data={contact} key={contact.name}/> 
+  );
   return (
     <div className="App">
       <Header />
-      {contacts.map(contact => 
-        <Contact data={contact} /> 
-      )}
+      {contactList}
     </div>
   );
 }
